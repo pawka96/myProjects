136740a94c6262c16f47686cf1341575cdc7f671
pawka96 <134133634+pawka96@users.noreply.github.com>
2023-05-20 11:47:21 +0300

    Add files via upload

---
+++ Game/src/DotComBustGame.java
@@ -0,0 +1,12 @@
+public class DotComBustGame {
+    /*
+     * Final playing class
+     */
+    public static void main(String[] args) {
+
+        DotComBust dotComGame = new DotComBust();
+        dotComGame.setUpGame();
+        dotComGame.startPlaying();
+        dotComGame.finishGame();
+    }
+}
